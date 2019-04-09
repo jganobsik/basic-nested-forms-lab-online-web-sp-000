d964d6f5633bb9a0eb107303f75859910e4283b5
Joe Ganobsik <jganobsik@gmail.com>
2019-04-09 15:14:23 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,4 +16,10 @@ class RecipesController < ApplicationController
      recipe = Recipe.create(recipe_params)
      redirect_to recipe
   end
+  
+   def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
 end
